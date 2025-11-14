cacb566d542bb86d86a9d7b49296d9020cf9665e
Nikhil Sharma <nikhil.sharma.studymail@gmail.com>
2025-11-14 16:35:06 +0530

    added Comments

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let boxex=document.querySelectorAll(".box");
 let resetbtn=document.querySelector("#reset");
 let winningText=document.querySelector(".winner");
-let newgamebtn=document.querySelector(".new-game");
+let newgamebtn=document.querySelector(".new-game-hidden-btn");
 let containerDiv=document.querySelector(".container");
 
+//all patterns where user can win
 const winPattern=[
     [0,1,2,"horizontal-up"],
     [3,4,5,"horizontal-middle"],
@@ -16,7 +17,7 @@ const winPattern=[
 ];
 
 
-let turnO=true;
+let turnO=true; //track the switching b/w players
 let count=0; //count how many buttons got clicked
 boxex.forEach((box)=>{
     box.addEventListener("click",()=>{
@@ -24,43 +25,43 @@ boxex.forEach((box)=>{
         if(turnO)
         {
             box.innerText="O";
-            turnO=false;
+            turnO=false;    //changing status
             count++;
         }
         else{
             box.innerText="X";
-            turnO=true;
+            turnO=true;     //changing status
             count++;
         }
         box.disabled=true;
 
-        if(checkWin())
+        if(checkWin())  //if someone won
         {
             showWinner();
             drawLine(arr[3]);
         }
-        else if(!checkWin()&&count==9)
+        else if(!checkWin()&&count==9) //if all button clicked but no one won
         {
             noWinner();
         }
     });
 });
 
-resetbtn.addEventListener("click",resetGame);
-newgamebtn.addEventListener("click",resetGame);
+resetbtn.addEventListener("click",resetGame);   //reset 
+newgamebtn.addEventListener("click",resetGame); //reset by new game btn
 
-function checkWin()
+function checkWin() //checks the win on every button click
 {
     for(arr of winPattern)
     {
-        let one=boxex[arr[0]].innerText;
-        let two=boxex[arr[1]].innerText;
-        let three=boxex[arr[2]].innerText;
+        let one=boxex[arr[0]].innerText;    //first button of pattern
+        let two=boxex[arr[1]].innerText;    //second button of pattern
+        let three=boxex[arr[2]].innerText;  //third button of pattern
         if(one!=""&&two!=""&&three!="") //checking non empty
         {
             if(one===two&&one===three)  //checkin equality
             {
-                return true;
+                return true;    
             }   
         }
     }
@@ -71,37 +72,41 @@ function showWinner()
     //since the turnO is now False
     if(turnO)
     {
-        winningText.innerText="Congratulations! Winner is X";
-        newgamebtn.className="show-new-game";
+        winningText.innerText="Congratulations! Winner is X";   //show winnig text
+        newgamebtn.className="show-new-game-btn";   //show new-game button
     }
     else{
-        winningText.innerText="Congratulations! Winner is O";
-        newgamebtn.className="show-new-game";
+        winningText.innerText="Congratulations! Winner is O";   //show winnig text
+        newgamebtn.className="show-new-game-btn";   //show new-game button
+    disableBoxes();
     }
 }
 
+//if no one won the game
 function noWinner()
 {
     winningText.innerText="No One Won!";
-    newgamebtn.className="show-new-game";
-}
+    newgamebtn.className="show-new-game-btn";
+};
+
+//function to reset game 
 function resetGame()
 {
     for(box of boxex)
     {
-        box.disabled=false;
-        box.innerText="";
-        winningText.innerText="";
-        newgamebtn.className="new-game";
-        hideLine();
+        box.disabled=false; //enable button click
+        box.innerText="";   //making button blank
+        winningText.innerText="";   //removing winning text
+        newgamebtn.className="new-game-hidden-btn";    //hidinng new-game button
+        hideLine(); //hiding the winning line
     }
-    count=0;
-    turnO=true;
-}
+    count=0;    //making count 0
+    turnO=true; //making game start from O
+};
 
 line=document.createElement("div");
 function drawLine(direction){
-    line.className=`${direction}`;
+    line.className=`${direction}`;  //adds line according the winning pattern
     // line.className="vertical-middle";
     // line.className="vertical-right";
     // line.className="horizontal-left";
@@ -112,16 +117,18 @@ function drawLine(direction){
     containerDiv.append(line);
 };
 
+//function for hiding line when game resets
 function hideLine()
 {
     line.className="hide-line";
     return;
-}
+};
 
+//function for disable the all boxex when someone win the game
 function disableBoxes()
 {
     for(box of boxex)   //disable all boxex
     {
         box.disabled=true;
     }
-}
+};
